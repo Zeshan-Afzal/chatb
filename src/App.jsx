5c22817c0781588686cf52beae86b437ac3a4f57
zeshan-afzal <zeshanafzal259@gmail.com>
2024-08-05 13:17:30 -0700

    Update API URLs in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useMemo, useState } from 'react'
 import {io} from 'socket.io-client'
 
 
-const socket= io("http://localhost:3000/")
+const socket= io("https://chats-tau.vercel.app")
 function App() {
 
   const [sendMessage, setSendMessage]=useState("")
