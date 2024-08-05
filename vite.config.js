b4c865ce25cf4a5ccacbe724083304a11c2d450b
zeshan-afzal <zeshanafzal259@gmail.com>
2024-08-05 15:57:08 -0700

    Update Vercel configuration for routing and CORS

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/socket.io': {
-        target: 'https://chats-tau.vercel.app',
+        target: 'https://chats-tau.vercel.app/socket.io',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/socket.io/, '/socket.io')
