f80203bc4bea1daeb8224a6db7e15f9d955fa13c
Zeshan Afzal <144064482+Zeshan-Afzal@users.noreply.github.com>
2024-08-08 09:14:33 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/socket.io': {
-        target: 'https://chats-tau.vercel.app/socket.io',
+        target: 'https://52cc-223-123-8-38.ngrok-free.app',
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/socket.io/, '/socket.io')
